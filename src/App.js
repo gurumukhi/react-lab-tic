a88a0c21c29d2a15bd6af291c25890f951b656f5
Ram Dayal Vaishnav <Ram@Tokirs-MacBook-Air.local>
2017-12-26 07:14:07 +0530

    one side flow using props working

--- src/App.js
+++ src/App.js
@@ -2,10 +2,28 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const playerEntriesAtSomePoint = [{
+  'player': 'X',
+  'x': 1,
+  'y': 2
+}, {
+  'player': 'O',
+  'x': 1,
+  'y': 0
+}, {
+  'player': 'X',
+  'x': 0,
+  'y': 0
+}];
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+  };
+
   render() {
     return (
-      <GameScreen />
+      <GameScreen entries = {playerEntriesAtSomePoint}/>
     );
   }
 }
@@ -13,11 +31,23 @@ class App extends Component {
 class GameScreen extends React.Component {
   render () {
     return <div>
-      <GridTable />
+      <GridTable entries = {this.props.entries}/>
       <br/>
-      <GameMessage />
+      <GameMessage entries = {this.props.entries}/>
     </div>;
   }
+}
+
+const getEntryAt = function (x, y, entries) {
+  console.log(entries);
+  if (!entries) {
+    return '.';
+  }
+  const useful = entries.filter(entry => entry.x === x && entry.y === y);
+  if(useful && useful.length){
+    return useful[0].player;
+  }
+  return '.';
 }
 
 class GridTable extends React.Component {
@@ -27,15 +57,15 @@ class GridTable extends React.Component {
 
   render () {
     return <div className="grid-table">
-      <GridCell />
-      <GridCell />
-      <GridCell /> <br/>
-      <GridCell />
-      <GridCell />
-      <GridCell /> <br/>
-      <GridCell />
-      <GridCell />
-      <GridCell />
+      <GridCell entry = {getEntryAt(0, 0, this.props.entries)} />
+      <GridCell entry = {getEntryAt(0, 1, this.props.entries)} />
+      <GridCell entry = {getEntryAt(0, 2, this.props.entries)} /> <br/>
+      <GridCell entry = {getEntryAt(1, 0, this.props.entries)} />
+      <GridCell entry = {getEntryAt(1, 1, this.props.entries)} />
+      <GridCell entry = {getEntryAt(1, 2, this.props.entries)} /> <br/>
+      <GridCell entry = {getEntryAt(2, 0, this.props.entries)} />
+      <GridCell entry = {getEntryAt(2, 1, this.props.entries)} />
+      <GridCell entry = {getEntryAt(2, 2, this.props.entries)} />
       </div>;
   }
 }
@@ -47,9 +77,17 @@ class GridCell extends React.Component {
 
   render () {
     return <span className='grid-cell'>
-      O
+      {this.props.entry}
       </span>;
   }
+}
+
+const getNextPlayer = function (entries) {
+  if(!entries || !entries.length) {
+    return 'X';
+  }
+  const lastEntryPlayer = entries[entries.length - 1].player;
+  return lastEntryPlayer === 'X' ? 'O' : 'X';
 }
 
 class GameMessage extends React.Component {
@@ -58,12 +96,12 @@ class GameMessage extends React.Component {
   }
 
   render () {
-    let currentMessage = 'Next player: X';
+    let currentMessage = 'Next player: ' + getNextPlayer(this.props.entries);
     return <div>
-        currentMessage
+        {currentMessage}
         <br/>
         <br/>
-        <LogEntry />
+        <LogEntry entries = {this.props.entries}/>
       </div>;
   }
 }
@@ -74,9 +112,22 @@ class LogEntry extends React.Component {
   }
 
   render () {
-    return <div>
-        <button>Goto step 1</button>: Game start.
-      </div>;
+    return (<div>
+              <div>
+                <button>Goto step 0</button>
+                <span> Game Start </span>
+                <br/><br/>
+              </div>
+        {
+          this.props.entries.map((entry, index) => {
+            return <div key={index}>
+              <button>Goto step {index}</button>
+              <span> move # {index+1} </span>
+              <br/><br/>
+              </div>
+          })
+        }
+      </div>);
   }
 }
 
