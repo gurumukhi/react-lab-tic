32d8cba61b444a0aaff1ac099bba36fd93f56b36
Ram Dayal Vaishnav <Ram@Tokirs-MacBook-Air.local>
2017-12-28 01:57:55 +0530

    added future scope

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 
+/*  Implement future scopes mentioned in https://reactjs.org/tutorial/tutorial.html#wrapping-up
+  * Display the location for each move in the format (col, row) in the move history list.
+  * Bold the currently selected item in the move list.
+  * Rewrite Board to use two loops to make the squares instead of hardcoding them.
+  * Add a toggle button that lets you sort the moves in either ascending or descending order.
+  * When someone wins, highlight the three squares that caused the win.
+*/
+
 class App extends Component {
   constructor(props) {
     super(props);
